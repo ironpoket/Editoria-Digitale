e536cc93a50258ef55e3b6161c26b5c954888c0b
ironpoket <ironpoket@gmail.com>
2023-02-16 15:34:29 +0100

    update

--- backend/navbar.php
+++ backend/navbar.php
@@ -30,17 +30,17 @@
           </li>
           <li class="dropdown"><a href="applicazioni.php"><span>App utili</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
-              <li><a href="#">Google Suite</a></li>
-              <li><a href="#">Whatsapp</a></li>
+              <li><a href="applicazioni.php#">Google Suite</a></li>
+              <li><a href="applicazioni.php#">Whatsapp</a></li>
               <li><a href="applicazioni.php#Stocard">Stocard</a></li>
-              <li><a href="#">Pharmaround</a></li>
-              <li><a href="#">Dove conviene</a></li>
-              <li><a href="#">Prezzi benzina</a></li>
-              <li><a href="#">IO</a></li>
-              <li><a href="#">App per il fitness</a></li>
+              <li><a href="applicazioni.php#pharmaround">Pharmaround</a></li>
+              <li><a href="applicazioni.php#dove-conviene">Dove conviene</a></li>
+              <li><a href="applicazioni.php#prezzi-benzina">Prezzi benzina</a></li>
+              <li><a href="applicazioni.php#io">IO</a></li>
+              <li><a href="applicazioni.php#">App per il fitness</a></li>
             </ul>
           </li>
-          <li><a class="nav-link scrollto" href="#">Pagamenti </a></li>
+          <li><a class="nav-link scrollto" href="payments.php">Pagamenti</a></li>
           <li><a class="nav-link scrollto" href="#">Ottimizzazione e sicurezza</a></li>
           
         <i class="bi bi-list mobile-nav-toggle"></i>
